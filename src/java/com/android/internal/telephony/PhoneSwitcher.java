9373daec23f8bcf4a089c640bd51e0e7e0e88c7a
zhaoyouyi <zhaoyouyi@allwinnertech.com>
2019-09-19 16:06:30 +0800

    Telephony: Fix out of boundary for non telephony device.
    Test: boot a wifi only device and check com.android.phone do not crash
    Bug: 141396302
    Change-Id: I4315f538a8a03a4b31b8ee77e16cc276ab712088
    Signed-off-by: zhaoyouyi <zhaoyouyi@allwinnertech.com>

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -879,6 +879,11 @@ public class PhoneSwitcher extends Handler {
     }
 
     private void switchPhone(int phoneId, boolean active) {
+        if (phoneId < 0 || phoneId >= mNumPhones) {
+            log("switchPhone, phoneId: " + phoneId +
+                ", mNumPhones: " + mNumPhones + ", should never here!");
+            return;
+        }
         PhoneState state = mPhoneStates[phoneId];
         if (state.active == active) return;
         state.active = active;
@@ -1094,6 +1099,11 @@ public class PhoneSwitcher extends Handler {
 
     @VisibleForTesting
     protected boolean isPhoneActive(int phoneId) {
+        if (phoneId < 0 || phoneId >= mNumPhones) {
+            log("isPhoneActive, phoneId: " + phoneId +
+                ", mNumPhones: " + mNumPhones + ", should never here!");
+            return false;
+        }
         return mPhoneStates[phoneId].active;
     }
 
